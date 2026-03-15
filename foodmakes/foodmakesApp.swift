6a402c515a1674ee3f3ad36dcca812e260d09c54
gorkemoa <gorkemoa35@gmail.com>
2026-03-15 12:40:07 +0300

    Initial Commit

---
+++ foodmakes/foodmakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodmakesApp.swift
+//  foodmakes
+//
+//  Created by Görkem Öztürk  on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct foodmakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ foodmakesTests/foodmakesTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodmakesTests.swift
+//  foodmakesTests
+//
+//  Created by Görkem Öztürk  on 15.03.2026.
+//
+
+import Testing
+@testable import foodmakes
+
+struct foodmakesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
